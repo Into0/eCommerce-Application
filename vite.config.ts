8d3292385c80be68f01ad42e3ac12f596d0b11cd
Into0 <Into0@users.noreply.github.com>
2025-05-05 00:20:24 +0300

    chore: setup vite
    #826806

---
+++ vite.config.ts
@@ -0,0 +1,16 @@
+import { defineConfig } from 'vite'
+
+export default defineConfig({
+  server: {
+    port: 3000,
+    open: false,
+  },
+  base: './',
+  root: './src',
+  publicDir: '../public',
+  build: {
+    emptyOutDir: true,
+    minify: true,
+    outDir: '../dist',
+  }
+})
