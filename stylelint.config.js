d442dc9ffe56aeff240ccd195b4a5396c49e2642
Into0 <Into0@users.noreply.github.com>
2025-05-16 00:41:50 +0300

    chore: add rule to ignore tailwindcss

--- stylelint.config.js
+++ stylelint.config.js
@@ -4,4 +4,12 @@ export default {
     'stylelint-config-clean-order',
     'stylelint-config-recommended-vue',
   ],
+  rules: {
+    'at-rule-no-unknown': [
+      true,
+      {
+        ignoreAtRules: ['tailwind'],
+      },
+    ],
+  },
 };
