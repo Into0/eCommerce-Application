d90779ebb36a91657f195b9226db3755991e6157
Into0 <Into0@users.noreply.github.com>
2025-05-16 00:26:34 +0300

    refactor: lint fix

--- stylelint.config.js
+++ stylelint.config.js
@@ -1,3 +1,7 @@
 export default {
-  extends: ["stylelint-config-standard", "stylelint-config-clean-order", "stylelint-config-recommended-vue"]
+  extends: [
+    'stylelint-config-standard',
+    'stylelint-config-clean-order',
+    'stylelint-config-recommended-vue',
+  ],
 };
